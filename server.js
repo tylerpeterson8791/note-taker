42ca80c56feb647733e7701269ecf4cc4ee87bd5
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-02 09:40:52 -0600

    Changed middleward to readFile.  Issue fixed

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-//THIS PROJECT DOESN'T LOOK LIKE I NEED TO BUILD ROUTES
-//IF I DO PUT app.use(routes) HERE
 
-// Routing
-// SEND / TO INDEX.  THIS MIGHT NOT EVEN BE NECESSARY
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
@@ -26,7 +22,6 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', injectData, (req, res) => {
-    ///API GET REQUEST OF NOTES. LINES 31-37 IN INDEX.JS
     const data = req.database.db;
     res.json(data);
 });
@@ -39,7 +34,6 @@ app.get('*', (req, res) =>
 
 // Routing (POST)
 app.post('/api/notes', injectData, (req, res) => {
-    ///HERE'S WHERE THE POST LOGIC GOES FOR API.  LINES 39-46 IN INDEX.JS
     //Grab fetch request
     const data = req.body;
     //Declare variable for all notes
@@ -52,8 +46,6 @@ app.post('/api/notes', injectData, (req, res) => {
     };
     //push new note into entire notes list
     notes.push(newNote);
-    //res with the new notes with the entry added
-    res.json(notes)
     //reqrite the db file with the added entry
     fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
         if (err) {
@@ -67,12 +59,10 @@ app.post('/api/notes', injectData, (req, res) => {
 
 // Routing (DELETE)
 app.delete('/api/notes/:id', injectData, (req, res) => {
-    //DELETE LOGIC GOES HERE. LINES 48-54 IN INDEX.JS!!!!!!!
     //Grab params of wildcard and declare variable
     const noteIdToDelete = req.params.id;
     //Declare whole database
     const notes = req.database.db;
-
     // Filter out the note with the specified ID.  Pass on anything not equal to noteToDelete
     const newNotes = notes.filter(note => note.id !== noteIdToDelete);
 
@@ -86,9 +76,7 @@ app.delete('/api/notes/:id', injectData, (req, res) => {
                 res.status(500).json({ error: 'Error. File not deleted.' });
             } else {
                 res.json({ message: "Note deleted successfully"});
-                //redirect them to the /notes page again so it refreshes with updated db on left side after record is removed
-                res.redirect('/notes');
-                return;
+                
             }
         });
     } 
@@ -99,48 +87,3 @@ app.delete('/api/notes/:id', injectData, (req, res) => {
 app.listen(PORT, () =>
     console.log(`Server running.  Click here: http://localhost:${PORT}`)
 );
-
-
-
-
-
-
-
-
-
-
-
-/*
-WHEN I open the Note Taker
-THEN I am presented with a landing page with a link to a notes page
-
-WHEN I click on the link to the notes page
-THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-
-WHEN I enter a new note title and the note’s text
-THEN a "Save Note" button and a "Clear Form" button appear in the navigation at the top of the page
-
-WHEN I click on the Save button
-THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes and the buttons in the navigation disappear
-
-WHEN I click on an existing note in the list in the left-hand column
-THEN that note appears in the right-hand column and a "New Note" button appears in the navigation
-
-WHEN I click on the "New Note" button in the navigation at the top of the page
-THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column and the button disappears
-
-I googled "How to create unique ID in java" into google and got this.  Make sure to credit: 
-
-First page found that sent me down rabbit hole
-https://stackoverflow.com/questions/2982748/create-a-guid-uuid-in-java
-Official NPM Page
-https://www.npmjs.com/package/uuidv4
-
-Filter Method
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-
-res.redirect()
-https://masteringjs.io/tutorials/express/redirect
-
-
-*/
